bae22152560d7512b3809d0762704fccc38f6f35
alanchrissantony <alanchrissantony@gmail.com>
2024-02-15 04:24:21 +0530

    initial

---
+++ authentication/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('home', views.home, name='home'),
+    path('signup', views.signup, name='signup'),
+    path('signin', views.login, name='signin'),
+    path('signout', views.signout, name='signout'),
+    path('root', views.root, name='root'),
+    path('roothome', views.roothome, name='roothome'),
+    path('edit/<int:id>', views.edit, name='edit'),
+    path('delete/<int:id>', views.delete, name='delete'),
+]
---
+++ authentication/views.py
@@ -0,0 +1,108 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.contrib import messages
+
+
+
+# Create your views here.
+
+def home(request):
+    if 'username' in request.session:
+        return render(request,"Youtube/index.html")
+    return redirect('signin')
+
+def roothome(request):
+    myUser = User.objects.all()
+    if request.method == "POST":
+        key = request.POST['search']
+        myUser = User.objects.filter(username__icontains=key)
+    return render(request,"account/admin.html",{'users':myUser})
+
+def login(request):
+        if 'username' in request.session:
+            return redirect('home')
+        if request.method == "POST":
+            email=request.POST['email']
+            password=request.POST["pass"]
+
+            user = authenticate(username=email, password=password)
+
+            if user is not None:
+                request.session['username']=str(user)
+                return render(request,"Youtube/index.html")
+            else:
+                messages.success(request, "Invalid Credentials")
+                return render(request,"account/login.html")
+   
+        return render(request,"account/login.html")
+
+def root(request):
+    if "admin" in request.session:
+        myUser = User.objects.all()
+        return render(request,"account/admin.html",{'users':myUser})
+    if request.method == "POST":
+        email=request.POST['email']
+        password=request.POST["pass"]
+        myUser = User.objects.filter(username=email).values('is_superuser')
+        if len(myUser) > 0:
+            myUser = myUser[0]
+            if myUser['is_superuser']:
+                user = authenticate(username=email, password=password)
+                if user is not None:
+                    request.session['admin']=str(user)
+                    myUser = User.objects.all()
+                    return render(request,"account/admin.html",{'users':myUser})
+                else:
+                    messages.success(request, "Invalid Credentials")
+                    return render(request,"account/admin-login.html")
+   
+    return render(request,"account/admin-login.html")
+
+
+def signup(request):
+    if 'username' in request.session:
+        return redirect('home')
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+        password = request.POST["pass"]
+        repassword = request.POST["re_pass"]
+        print(username)
+        if password==repassword:
+            try:
+                myUser = User.objects.create_user(email, username, password)
+                myUser.name = username
+                myUser.save()
+                messages.success(request, "Your account has been successfully created.")
+                return redirect('signin')
+            except Exception as error:
+                messages.success(request, "User already exits")
+                return redirect('signup')
+
+            
+        else:
+            messages.error(request,"Password doesn't match")
+    return render(request,"account/signup.html")
+
+def signout(request):
+    request.session.clear()
+    return redirect('signin')
+
+def edit(request, id):
+    myUser = User.objects.get(id=id)
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        myUser.username = email
+        myUser.email = username
+        myUser.save()
+        myUser = User.objects.all()
+        return render(request,"account/admin.html",{'users':myUser})
+    return render(request,"account/edit.html",{'users':myUser})
+
+def delete(request, id):
+    myUser = User.objects.get(id=id)
+    myUser.delete()
+    myUser = User.objects.all()
+    return render(request,"account/admin.html",{'users':myUser})
